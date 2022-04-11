4d4e290a4f720b83633e7c42c9804239a20809f2
Precious <preciousmaregwa1997@gmail.com>
2022-04-11 11:49:00 +0300

    complete assigment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const employee = {
+    name : "sam",
+    streetAddress : "11 broadway"
+};
+const updateEmployeeWithKeyAndValue = (employee,key,value) => {
+    return {
+        ...employee,
+        [key] :value,
+    };
+};
+const destructivelyUpdateEmployeeWithKeyAndValue = (employee,key,value) =>{
+    employee[key] =  value;
+    return employee
+};
+const deleteFromEmployeeByKey =(employee,key) =>{
+    const newEmployee = {...employee};
+    delete newEmployee[key];
+    return newEmployee
+};
+const destructivelyDeleteFromEmployeeByKey = (employee,key) =>{
+    delete employee[key];
+    return employee
+}
+
